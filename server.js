87039f84daae6440a089f9b18dd727c44f6b5aa8
georgematos <georgemattos@gmail.com>
2020-08-07 11:21:01 -0300

    heroku configs to deploy

---
+++ server.js
@@ -0,0 +1,15 @@
+//Install express server
+const express = require('express');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/translater'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile('index.html',{root: '/dist/translater/'});
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
