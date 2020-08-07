317ed7df87b0dc55befe2bc5e4d67b1f7426f7e5
georgematos <georgemattos@gmail.com>
2020-08-07 04:18:48 -0300

    adding cors-anywhere.herokuapp to urlApi

--- src/app/services/translate-service.service.ts
+++ src/app/services/translate-service.service.ts
@@ -6,9 +6,9 @@ import { catchError } from 'rxjs/operators';
 @Injectable()
 export class TranslateService {
 
-  private urlApi = 'https://translate.google.com/translate_a/single?' + 
+  private urlApi = 'https://cors-anywhere.herokuapp.com/translate.google.com/translate_a/single?' + 
   'client=at&dt=t&dt=ld&dt=qca&dt=rm&dt=bd&dj=1&hl=%25s&ie=UTF-8&oe=UTF-8&inputm=2&otf=2&iid=1dd3b944-fa62-4b55-b330-74909a99969e&';
-  // cors-anywhere.herokuapp.com/
+
   constructor(
     private httpClient: HttpClient
   ) { }
